0f522acb47f222533cac3d3f2418d2132bfb1aba
Suraj <contact@suninthesky.co.uk>
2016-05-10 16:59:10 +0100

    Add question data

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class notApp extends Component {
-  render () {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+          <div>{this.props.children}</div>
+        );
+    }
 }
+
+notApp.propTypes = {
+    children: React.PropTypes.object.isRequired,
+};
--- imports/ui/Question.jsx
+++ imports/ui/Question.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default class Question extends React.Component {
-  render () {
-    return (
-      <div>
-        <label>this is a question</label>
-        <textarea placeholder='answer'></textarea>
-      </div>
-    )
-  }
+    render() {
+        return (
+          <div>
+            <label>{this.props.question}</label>
+            <textarea placeholder="Answer"></textarea>
+          </div>
+        );
+    }
 }
